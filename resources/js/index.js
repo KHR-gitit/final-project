52e2c8e94dcdb7754aabc0429001df59cd1514f5
soumya <ksoumya3001@gmail.com>
2021-02-10 10:22:11 +1100

    add logic validation of task 4.1

--- resources/js/index.js
+++ resources/js/index.js
@@ -9,9 +9,40 @@ const deleteTask = document.querySelector('#delete-task');
 
 addTaskForm.addEventListener('submit', e => {
     e.preventDefault();
-    console.log(`Task : ${taskName.value};`);
-    console.log(`Assigned to : ${assignedTo.value};`);
-    console.log(`Description : ${description.value};`);
-    console.log(`Due date : ${dueDate.value};`);
-    console.log(`Status : ${taskStatus.value};`);
+   
+//Check if the Task Name input value is more than 5 characters.
+if (taskName.value.length > 5) {
+    console.log("Valid:Task Name is more than 5 characters")
+   } else {
+    console.log("Invalid:Task name has less than 5 Characters")
+
+}
+
+if (assignedTo.value.length > 5) {
+    console.log("Valid:Task assigned to is more than 5 characters")
+} else {
+    console.log("Invalid:Task assigned to is less than 5 Characters")
+}
+
+if (description.value.length > 5) {
+    console.log("Valid:Task description is more than 5 characters")
+} else {
+    console.log("Invalid:Task description is less than 5 Characters")
+}
+
+if (dueDate.value || dueDate !== Date.now() ) {
+    console.log("Valid:Date is valid")
+} else {
+    console.log("Invalid:Date is invalid")
+}
+
 })
+
+
+
+
+
+
+
+
+ 
