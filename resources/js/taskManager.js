96d0a397ecf9e24de107afd8a2069af6f3ea1e97
swathimuroor <swathimuroor@gmail.com>
2021-02-12 09:22:48 +1100

    add task07.1

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,47 +1,40 @@
-class TaskManager {
-    constructor (currentId = 0){
-        this.tasks = [];
-        this.currentId = currentId;
-    
-    }
-
-//addTask Method
-addTask(name, description,assignedTo, dueDate, status) {
-
-    const task = {
-    id: this.currentId++,
-    name: name,
-    description: description,
-    assignedTo:  assignedTo,
-    dueDate: dueDate,
-    status: status
-
-};
-this.tasks.push(task);
-
-}
-createTaskHtml(id, name, description, assignedTo, dueDate, status){
+const createTaskHtml = (id, taskName, description, assignedTo, dueDate, status) => {
     let color;
     let icon;
+    let btnTxt = "Done";
+    let btnLine="";
     if(status === 'To Do') {
         color = 'primary';
         icon = 'bi-hourglass';
-    } else if(status === 'In progress') {
+        btnTxt = "In Progress";
+        btnLine = `<button class="btn btn-success task-done-btn" > ${btnTxt}</button>`;
+    } else if(status === 'In Progress') {
         color = 'success';
         icon = 'bi-gear-fill';
+        btnTxt = "Review";
+        btnLine = `<button class="btn btn-success task-done-btn" > ${btnTxt}</button>`;
     } else if(status === 'Review') {
         color = 'warning';
         icon = 'bi-check-all';
+        btnTxt = "Done";
+        btnLine = `<button class="btn btn-success task-done-btn" > ${btnTxt}</button>`;
     } else if(status === 'Done') {
         color = 'info';
         icon = 'bi-check2-square';
+        btnTxt = "Complete";
+        btnLine = `<button class="btn btn-success task-done-btn" > ${btnTxt}</button>`;
+    }else if(status === 'Completed'){
+        color = 'info';
+        icon = 'bi-check2-square';
+        btnTxt = "Complete";
+        btnLine = `<button class="btn btn-success task-done-btn invisible" > ${btnTxt}</button>`;
     }
     const html = `
                 <li class="list-group-item border-0" id="${id}">
                     <div class="card text-${color} w-100 border-${color} mb-3">
                     <div class="card-header  d-flex justify-content-between">
                         <div class="task-status"> <i class="bi ${icon}"></i>${status}</div>
-                        <h5 class="card-title ">${name}</h5>
+                        <h5 class="card-title ">${taskName}</h5>
                         <div class="assigned-to ">
                         <h5 class="display-6 fs-5"> <i class="bi bi-person-square"></i> <b>${assignedTo}</b></h5>
                         </div>
@@ -51,29 +44,57 @@ createTaskHtml(id, name, description, assignedTo, dueDate, status){
                     </div>
                     <div class="card-footer d-flex">
                         <span class="date">Due Date: ${getDateInFormat(new Date(dueDate))}</span>
-                        <div class="button-box  ms-auto">
-                        <button class="btn btn-success"> Done</button>
-                        <button class="btn btn-danger">Delete</button>
+                        <div class="button-box  ms-auto">  `+ btnLine+
+                        `  <button class="btn btn-danger task-delete-btn">Delete</button>
                         </div>
                     </div>
                     </div>
-                </li>
-                 `
+                </li>`
                  return html;
 }
 
-render() {
-    const taskHtmlList = [];
-    for(let i = 0; i < this.tasks.length; i++) {
-        let task = this.tasks[i];
-        taskHtmlList.push(this.createTaskHtml(task.id, task.name, task.description, task.assignedTo, task.dueDate, task.status))
+class TaskManager {
+
+    constructor (currentId = 0){
+        this.tasks = [];
+        this.currentId = currentId;
     }
-    let taskHtml = taskHtmlList.join('\n');
-    document.querySelector('#listMenu').innerHTML = taskHtml;
-}
 
+    //addTask Method
+    addTask(name, description,assignedTo, dueDate, status) {
+        const task = {
+            id: this.currentId++,
+            name: name,
+            description: description,
+            assignedTo:  assignedTo,
+            dueDate: dueDate,
+            status: status
+        };
+        this.tasks.push(task);
+    }
+    
+    // display task
+    render() {
+        const taskHtmlList = [];
+        for(let i = 0; i < this.tasks.length; i++) {
+            let task = this.tasks[i];
+            taskHtmlList.push(createTaskHtml(task.id, task.name, task.description, task.assignedTo, task.dueDate, task.status))
+        }
+        let taskHtml = taskHtmlList.join('\n');
+        document.querySelector('#listMenu').innerHTML = taskHtml;
+    }
 
-
+    // get the task object
+    getTaskById(taskId){
+        let foundTask ;
+        for(let i = 0; i < this.tasks.length; i++) {
+            let task = this.tasks[i];
+            if(task.id == taskId){
+                foundTask = task;
+            }
+        }
+        return foundTask; 
+    }
 
 }
 
